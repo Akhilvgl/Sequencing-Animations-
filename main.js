934edd43a5e82ce1138f6579226e94a5be5824af
Akhilvgl <147181194+Akhilvgl@users.noreply.github.com>
2023-10-21 06:29:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const aliceTumbling = [
+const spTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
 ];
 
-const aliceTiming = {
+const spTiming = {
     duration: 2000,
     iterations: 1,
     fill: 'forwards'
 }
 
-const alice1 = document.querySelector("#alice1");
-const alice2 = document.querySelector("#alice2");
-const alice3 = document.querySelector("#alice3");
+const sp1 = document.querySelector("#sp1");
+const sp2 = document.querySelector("#sp2");
+const sp3 = document.querySelector("#sp3");
 
-alice1.animate(aliceTumbling, aliceTiming).finished
-    .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
-    .then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
-    .catch(error => console.error(`Error animating Alices: ${error}`));
+sp1.animate(spTumbling, spTiming).finished
+    .then(() => sp2.animate(spTumbling, spTiming).finished)
+    .then(() => sp3.animate(spTumbling, spTiming).finished)
+    .catch(error => console.error(`Error animating sps: ${error}`));
